c1b7b9b6567552e68f5f27196c09f49d95520577
Joonsang <jsyoo1@twayair.com>
2020-10-23 17:25:20 +0900

    2020.10.23 dependency.gradle 생성 및 라이브러리 정리

--- app/src/main/java/com/dream/dreamteller/BaseApplication.kt
+++ app/src/main/java/com/dream/dreamteller/BaseApplication.kt
@@ -3,18 +3,21 @@ package com.dream.dreamteller
 import android.app.Application
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
-
+/**
+ * Created by joonsangyoo on 2020. 10. 21..
+ */
 class BaseApplication : Application(){
 
     override fun onCreate() {
         super.onCreate()
 
+        // 디버그 모드일때만 로그 남기기 위함
         if (BuildConfig.DEBUG) {
             //AppLogger.init()
             //Timber.plant(Timber.DebugTree())
         }
 
-        // start Koin!
+        // 코인 연동부분..
         startKoin {
             // Android context
             androidContext(this@BaseApplication)
